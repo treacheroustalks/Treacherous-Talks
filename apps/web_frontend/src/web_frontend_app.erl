ade8e3e82aee39b97b77ec2feef3932c0402b92f
Stephan Brandauer <stephan.brandauer@gmail.com>
2011-10-20 17:11:46 +0200

    [#131] web_frontend now net_adm:pings the backend during startup
    It will fail, if it is started and there is now backend running.
    It produces a readable error message. Look for it.
    
    fix a bug in user_record_gen - used to store everything as binary
    
    create_user now works for the first time as well, pg2:start_link() in
    the app startup was necessary.
    
    Change-Id: I3c2de6e8922c2acb019d85ba78836582bec99e05

--- apps/web_frontend/src/web_frontend_app.erl
+++ apps/web_frontend/src/web_frontend_app.erl
@@ -7,6 +7,13 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    io:format ("starting [~p]~n", [?MODULE]),
+    pg2:start_link (),
+    case net_adm:ping('backend@127.0.0.1') of
+        pang ->
+            erlang:error ({error, could_not_ping_hardcoded_backend});
+        pong -> ok
+    end,
     web_frontend_sup:start_link().
 
 stop(_State) ->
