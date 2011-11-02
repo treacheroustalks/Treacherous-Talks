2aecbb1316542cf338f95573c3922f19e3174f2b
Erik Timan <dev@timan.info>
2011-11-02 15:56:43 +0100

    Add real register and login Selenium tests
    Change-Id: I88e3df8f4cd999efd2abe3067dc197e91b6b777c

--- ext_test/web_interface_test.py
+++ ext_test/web_interface_test.py
@@ -4,28 +4,52 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 import time
 
-def basicWebsocket(driver, path):
-    testString = "Hello World from test!"
-    driver.get(path)
-    
-    # Find the 'Connect' button and click it to connect
-    connectButton = driver.find_element_by_id("cA")
-    connectButton.click()
-    
-    # Chat
-    inputField = driver.find_element_by_id("phrase")
-    sendButton = driver.find_element_by_id("sendB")
-    inputField.send_keys(testString)
-    sendButton.click()
-    
-    try:
-        # Check that we have received an echo
-        xpath = "//div[@id='msgs']/span[1]"
-        assert testString == driver.find_element_by_xpath(xpath).text
-        
-    finally:
-        driver.quit()
+address="http://localhost:8000"
 
+### Generalized test functions
+def registerUser(driver, path, data, response):
+    driver.get(path)
+    time.sleep(1)
+
+    driver.find_element_by_partial_link_text("Register!").click()
+    time.sleep(1)
+
+    for element, value in data.iteritems():
+        driver.find_element_by_id(element).send_keys(value)
+
+    driver.find_element_by_xpath("//form[@id='register']/p[7]/button").click()
+    time.sleep(1)
+
+    assert response == driver.find_element_by_class_name("success").text
+
+def login(driver, path, data, response):
+    driver.get(path)
+    time.sleep(1)
+
+    for element, value in data.iteritems():
+        driver.find_element_by_id(element).send_keys(value)
+
+    driver.find_element_by_xpath("//form[@id='login']/p[4]/button").click()
+    time.sleep(1)
+
+    assert response == driver.find_element_by_xpath("//div[@id='page']/h2").text
+
+
+### Tests
+def registerUserTest(driver):
+    data = {"reg_email": "a@a.com",
+            "reg_fullname": "Aa Aa",
+            "reg_nick": "aa",
+            "reg_password": "1234",
+            "reg_confirm_password": "1234"}
+    response = "Registration successful. Please login to continue."
+    registerUser(driver, address, data, response)
+
+def loginTest(driver):
+    data = {"login_nick": "aa",
+            "login_password": "1234"}
+    response = "Welcome to the game"
+    login(driver, address, data, response)
 
 # Create a new (shared among tests) browser instance
 driver = webdriver.Chrome()
@@ -33,9 +57,11 @@ driver = webdriver.Chrome()
 # Keep the try/except blocks around the tests so that we can close the browser
 # properly even when tests fail :-)
 try:
-    # Run the test for websocket example
-    basicWebsocket(driver, "http://localhost:8000/websockets_example.yaws")
+    registerUserTest(driver)
+    loginTest(driver)
 except Exception, e:
     # Terminate the browser and re-throw the exception
     driver.quit()
     raise
+
+driver.quit()
