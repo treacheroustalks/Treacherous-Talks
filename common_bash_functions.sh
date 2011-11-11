cdb9afbc5b4519271315cf21d8a24a60f757e209
Erik Timan <dev@timan.info>
2011-11-11 10:59:03 +0100

    Only colorize bash output if running in TTY
    Change-Id: I750f18d512f58eae421813532e63ce6522ab8c47

--- common_bash_functions.sh
+++ common_bash_functions.sh
@@ -6,10 +6,16 @@
 # command. Warning! This is a bash specific feature.
 set -o pipefail
 
-# Color definitions
-c_red="\033[31m"
-c_brown="\033[33m"
-c_reset="\033[0m"
+# Set some color definitions, but only if we're running in a real terminal
+if [ -t 1 ]; then
+    c_red="\033[31m"
+    c_brown="\033[33m"
+    c_reset="\033[0m";
+else
+    c_red=""
+    c_brown=""
+    c_reset="";
+fi
 
 # Description: Echoes text in red color with ##### signs in front to STDERR
 # First argument: text to echo
