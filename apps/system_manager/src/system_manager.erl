2644fb66dc3fa7c51d22684643d9f5e81923de9a
Erik Timan <dev@timan.info>
2011-11-23 14:03:08 +0100

    [#357] Add skeleton of system manager application
    This adds a system manager app that is supposed to be run in a separate
    VM and to be used for configuring the system. The idea is that one can
    connect to the system manager and dynamically change configuration of a
    server and also start and stop releases. This is only a skeleton without
    any functional code.
    
    Change-Id: I497871b36b494cbcf9152dfb26098a4dd6470720

---
+++ apps/system_manager/src/system_manager.erl
@@ -0,0 +1,84 @@
+%%%-------------------------------------------------------------------
+%%% @copyright
+%%% Copyright (C) 2011 by Bermuda Triangle
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%%% of this software and associated documentation files (the "Software"), to deal
+%%% in the Software without restriction, including without limitation the rights
+%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%%% copies of the Software, and to permit persons to whom the Software is
+%%% furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in
+%%% all copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%%% THE SOFTWARE.
+%%% @end
+%%%-------------------------------------------------------------------
+%%% @author Erik Timan <erol7391@student.uu.se>
+%%%
+%%% @doc The system manager application handles configuration of the Treacherous
+%%% Talks system. It is intended to be the single point of entry for configuring
+%%% and controlling the whole system on a server.
+%%%
+%%% @end
+%%%
+%%% @since : 23 Nov 2011 by Bermuda Triangle
+%%% @end
+%%%-------------------------------------------------------------------
+-module(system_manager).
+
+-behaviour(gen_server).
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+-export([start_link/0]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link(?MODULE, no_arg, []).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init(no_arg) ->
+    {ok, {}}.
+
+handle_call(ping, _From, State) ->
+    {reply, {pong, self()}, State}.
+
+handle_cast(_Msg, State) ->
+    io:format ("received unhandled cast: ~p~n",[{_Msg, State}]),
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    io:format ("[~p] terminated ~p: reason: ~p, state: ~p ~n",
+               [?MODULE, self(), _Reason, _State]),
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Functions
+%% ------------------------------------------------------------------
